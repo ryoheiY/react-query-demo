20a86c2ab7d87db6cb486dca371b9e930413e708
ryode <ryo.dev.tech@outlook.com>
2024-01-04 19:00:27 +0900

    init

---
+++ src/components/Events/EditEvent.jsx
@@ -0,0 +1,77 @@
+import {Link, redirect, useNavigate, useNavigation, useParams, useSubmit} from 'react-router-dom';
+
+import Modal from '../UI/Modal.jsx';
+import EventForm from './EventForm.jsx';
+import {useQuery} from "@tanstack/react-query";
+import {fetchEvent, queryClient, updateEvent} from "../../util/http.js";
+import ErrorBlock from "../UI/ErrorBlock.jsx";
+
+export default function EditEvent() {
+    const navigate = useNavigate();
+    const {state} = useNavigation();
+    const submit = useSubmit();
+    const params = useParams();
+    const {data, isError, error} = useQuery({
+        queryKey: ["events", params.id],
+        queryFn: ({signal}) => fetchEvent({signal, id: params.id}),
+        staleTime: 10000
+    });
+
+    function handleSubmit(formData) {
+        submit(formData, {method: "PUT"});
+    }
+
+    function handleClose() {
+        navigate('../');
+    }
+
+    let content;
+
+    if (isError) {
+        content = (
+            <>
+                <ErrorBlock title="Failed to load event" message={error.info?.message || "Failed to load event!?"}/>
+                <div className="form-actions">
+                    <Link to="../" className="button">
+                        OKay
+                    </Link>
+                </div>
+            </>
+        );
+    }
+
+    if (data) {
+        content = (<EventForm inputData={data} onSubmit={handleSubmit}>
+            {state === "submitting" ? <p>Sending data..</p> :
+                <><Link to="../" className="button-text">
+                    Cancel
+                </Link>
+                    <button type="submit" className="button">
+                        Update
+                    </button>
+                    )</>}
+
+        </EventForm>)
+    }
+
+    return (
+        <Modal onClose={handleClose}>
+            {content}
+        </Modal>
+    );
+}
+
+export function loader({params}) {
+    return queryClient.fetchQuery({
+        queryKey: ["events", params.id],
+        queryFn: ({signal}) => fetchEvent({signal, id: params.id})
+    });
+}
+
+export async function action({request, params}) {
+    const formData = await request.formData();
+    const updatedEventData = Object.fromEntries(formData);
+    await updateEvent({id: params.id, event: updatedEventData});
+    await queryClient.invalidateQueries(["events"]);
+    return redirect("../");
+}
